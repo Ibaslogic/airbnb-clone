9062e1d00b74690fc49325641179e87d64e96bac
ibaslogic <ibaskunle@gmail.com>
2023-06-18 19:22:47 +0100

    Add first commit

---
+++ app/libs/prismadb.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+
+export default prisma;
+
+// import { PrismaClient } from '@prisma/client'
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined
+// }
+
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({
+//     log: ['query'],
+//   })
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
